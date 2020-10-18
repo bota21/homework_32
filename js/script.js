8cc8cb848c9e09004d7f7c4eeb6f068ed4df3c40
botagoz <botagoz.aimkulova@gmail.com>
2020-10-18 20:25:34 +0600

    #1 Создана основная контентная часть страницы

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(() => {
   $('.carousel').slick({
     infinite: true,
     dots: false,
-    // autoplay: true,
-    // autoplaySpeed: 3000,
+    autoplay: true,
+    autoplaySpeed: 3000,
     arrows: false
 
   });
